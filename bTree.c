c5b9f02fbe42371d399ea91866fe9ea8fd59fa3d
Lakshya Chopra <lakshyachopra2707@gmail.com>
2024-02-27 23:20:41 +0530

    UPDATE

---
+++ bTree.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct node{
+	int val;
+	struct node * lChild;
+	struct node * rChild;
+}bTree;
+
+const int sizeOfTree = sizeof(bTree);
+
+bTree * root = NULL;
+
+int main(){
+
+	return 0;
+}
